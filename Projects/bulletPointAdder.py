ff6051e07efe3e011235c751c97db62359d07522
Lance Giles <lmgiles6@gmail.com>
2017-09-19 03:52:51 +0000

    add randomQuizGenerator project

--- Projects/bulletPointAdder.py
+++ Projects/bulletPointAdder.py
@@ -5,6 +5,10 @@
 import pyperclip
 text = pyperclip.paste()
 
-#TODO separate lines and add stars
+# separate lines and add stars
+lines = text.split('\n')
+for i in range(len(lines)):
+  lines[i] = '* ' + lines[i]
+text = '\n'.join(lines)
 
 pyperclip.copy(text)
